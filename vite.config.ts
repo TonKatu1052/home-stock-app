a0d6cfcf49611f4b94ff140f5fd2235b0d2ff9a2
TonKatu11029 <porkcutlet11029@gmail.com>
2026-04-20 06:05:53 +0900

    icon追加

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,18 @@ export default defineConfig({
         display: 'standalone',
         background_color: '#000000',
         theme_color: '#000000',
-        icons: [],
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'icon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
       },
     }),
   ],
